9b502f082e9e6124fb94faabae9663665676b5da
Corb Sergiu <corbsergiu@yahoo.com>
2017-04-28 17:37:39 +0300

    up1st

---
+++ assets/jquery/jquery.js
@@ -0,0 +1,78 @@
+
+var playing = false;
+var fruits = ["apple", "banana", "cherry", "grape", "orange", "peach", "pineapple", "watermelon"];
+var score;
+var trialsleft;
+var action;
+$(function () {
+    $("#start-reset").click(function () {
+        if (playing == true) {
+            location.reload();
+        } else {
+            playing = true;
+            score = 0;
+            $("#scorevalue").html(score);
+            $("#trialsleft").show();
+            $("#start-reset").html("Reset Game");
+            trialsleft = 3;
+            addHearts();
+            startAction();
+            moveFruitDown();
+            startInterval();
+        };
+    });
+    if ($("#fruit1").position().top >= 400 + "px") {
+        $("#container #trialsleft  img:last-child").remove();
+    };
+    function addHearts() {
+        for (i = 0; i < trialsleft; i++) {
+            $("#trialsleft").append('<img src="images/heart.png" class="life">');
+        };
+    };
+    function startAction() {
+        $("#fruit1").show();
+        $("#fruit1").css({
+            position: "absolute",
+            left: Math.floor((Math.random() * 530)) + "px",
+            top: -100 + "px"
+        });
+        chooseFruit();
+    };
+    function startInterval() {
+        action = setInterval(function () {
+            if (trialsleft > 0) {
+                startAction();
+                moveFruitDown();
+            } else {
+                playing = true;
+                $("#trialsleft").hide();
+                $("#game-over").show();
+                $("#finalScore").html(score);
+                stop();
+            };
+        }, 1700);
+    };
+    $("#fruit1").mouseover(function () {
+        trialsleft = 3;
+        score++;
+        $("#scorevalue").html(score);
+        $(this).hide();
+    });
+    function chooseFruit() {
+        $("#fruit1").attr("src", "images/" + fruits[Math.floor((Math.random() * 8))] + ".png");
+    };
+    function moveFruitDown() {
+        $("#fruit1").animate({
+            top: 550 + "px"
+        }, 1400, function () {
+            if ($("#fruit1").position().top > $("#fruitsContainer").height()) {
+                $("#container #trialsleft  img:last-child").remove();
+                trialsleft--;
+            };
+        });
+    };
+    function stop() {
+        $("#fruit1").hide();
+        clearInterval(action);
+    };
+});
